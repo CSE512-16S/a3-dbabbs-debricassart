ffd61e2676960d1e054caca13f02c878177a320d
Dylan Babbs <dylan.babbs@gmail.com>
2016-05-02 14:04:59 -0700

    Delete variables.js

--- variables.js
+++
@@ -1,6 +0,0 @@
-//var mode = Array.apply(0, Array(20)).map(function (x, y) { return y + 1; });;
-var mode = Array.apply(0, Array(5)).map(function (x, y) { return y + 1; });;
-var purpose = Array.apply(0, Array(4)).map(function (x, y) { return y + 1; });;
-//var mode = ['walk','drive','bicycle','bus','other']
-//var purpose = ['work','home','shopping','other'];
-var hour = 'all';
