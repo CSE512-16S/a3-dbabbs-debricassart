0070229de15534ba029ca6ac5448ba44b3b1ddaa
Dylan Babbs <dylan.babbs@gmail.com>
2016-05-01 23:26:11 -0700

    prepping for submission

---
+++ variables.js
@@ -0,0 +1,6 @@
+//var mode = Array.apply(0, Array(20)).map(function (x, y) { return y + 1; });;
+var mode = Array.apply(0, Array(5)).map(function (x, y) { return y + 1; });;
+var purpose = Array.apply(0, Array(4)).map(function (x, y) { return y + 1; });;
+//var mode = ['walk','drive','bicycle','bus','other']
+//var purpose = ['work','home','shopping','other'];
+var hour = 'all';
